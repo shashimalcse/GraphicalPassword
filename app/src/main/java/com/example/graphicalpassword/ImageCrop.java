38346c5fbddc211fc1ad795d8615d58082805355
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2020-03-11 19:29:13 +0530

    add the crop libarary

---
+++ app/src/main/java/com/example/graphicalpassword/ImageCrop.java
@@ -0,0 +1,13 @@
+package com.example.graphicalpassword;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageCrop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_crop);
+    }
+}
