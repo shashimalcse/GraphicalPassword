2fec36d838ce1d589fba6ff0e6be2761c2be8c9d
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2020-03-11 19:29:39 +0530

    add the crop libarary

--- app/src/main/java/com/example/graphicalpassword/ImageCrop.java
+++ app/src/main/java/com/example/graphicalpassword/ImageCrop.java
@@ -1,13 +1,50 @@
 package com.example.graphicalpassword;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.theartofdev.edmodo.cropper.CropImage;
 
 public class ImageCrop extends AppCompatActivity {
+
+    ImageView imageview;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_crop);
+
+        imageview = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CropImage.activity().setAspectRatio(1,1).start(ImageCrop.this);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+
+                imageview.setImageURI(resultUri);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 }
